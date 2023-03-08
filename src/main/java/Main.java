6657197742002fafbf7c83ac6561a73670687373
ikvashnin <i89998004607@gmail.com>
2023-03-08 19:48:31 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String x = new String("test");
+        x = "test 2";
+        String y = new String("test 2");
+        System.out.println(x.equals(y));
+    }
+}
