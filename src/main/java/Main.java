c0283f75de29ac44bf1c69acdef1fde9d30f04f8
ikvashnin <i89998004607@gmail.com>
2023-03-08 19:55:40 +0300

    test-3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println(x.equals(y));
         System.out.println(x.equals(y));
         System.out.println(x.equals(y));
+        System.out.println(x.equals(y));
     }
 }
